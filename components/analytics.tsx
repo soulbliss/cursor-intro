476d4866062bb506ca305a43271aa912706f4348
deeps.dev <deepak.garasangi@gmail.com>
2025-02-19 14:21:03 +0530

    add clarity

--- components/analytics.tsx
+++ components/analytics.tsx
@@ -2,8 +2,6 @@
 
 import Script from 'next/script';
 
-// Uses Google Analytics, replace with your own analytics provider
-
 export function Analytics() {
   return (
     <>
@@ -11,6 +9,15 @@ export function Analytics() {
         defer
         data-domain="cursorintro.com"
         src="https://analytics.deeps.dev/js/script.js"></Script>
+      <Script id="microsoft-clarity">
+        {`
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "qc6xu4rb1f");
+        `}
+      </Script>
     </>
   );
 }
